b3a275ffcc5db8630062492e3735d5a4dd669154
Jason <jasonmorales88j@gmail.com>
2026-02-22 18:12:28 -0800

    new lesson

--- rust/rust-practice/src/main.rs
+++ rust/rust-practice/src/main.rs
@@ -1,75 +1,66 @@
 #[derive(Debug)]
-struct Circle {
-    radius: f64,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-impl Circle {
-    fn new(radius: f64) -> Circle {
-        Circle { radius }
-    }
-
-    fn area(&self) -> f64 {
-        self.radius.powi(2) * std::f64::consts::PI
-    }
-
-    fn circumference(&self) -> f64 {
-        2.0 * std::f64::consts::PI * self.radius
-    }
-}
-
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u32,
-}
-
-impl Person {
-    fn new(name: &str, age: u32) -> Person {
-        Person {
-            name: name.to_string(),
-            age,
-        }
-    }
-
-    fn greet(&self) -> String {
-        format!("Hi, I'm {} and I'm {} years old.", self.name, self.age)
+fn value_in_cents(coin: &Coin) -> u32 {
+    match coin {
+        Coin::Penny => return 1,
+        Coin::Nickel => return 5,
+        Coin::Dime => return 10,
+        Coin::Quarter => return 25,
     }
 }
 
 #[derive(Debug)]
-struct Stack(Vec<i32>);
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(u8, u8, u8),
+}
 
-impl Stack {
-    fn new() -> Stack {
-        Stack(Vec::new())
+fn process(msg: &Message) {
+    match msg {
+        Message::Quit => println!("Quitting"),
+        Message::Move { x, y } => println!("Moving to ({}, {})", x, y),
+        Message::Write(str) => println!("Writing: {}", str),
+        Message::ChangeColor(r, g, b) => println!("Changing color to rgb({}, {}, {})", r, g, b),
     }
+}
 
-    fn push(&mut self, val: i32) {
-        self.0.push(val);
-    }
-
-    fn pop(&mut self) -> Option<i32> {
-        self.0.pop()
-    }
-
-    fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
+fn divide(a: f64, b: f64) -> Option<f64> {
+    if b == 0.0 { None } else { Some(a / b) }
 }
 
 fn main() {
-    let circle = Circle::new(5.0);
-    println!("{:?}", circle);
-    println!("{}", circle.area());
-    println!("{}", circle.circumference());
+    let coins = [Coin::Penny, Coin::Nickel, Coin::Dime, Coin::Quarter];
+    for coin in &coins {
+        println!("{:?} = {} cents", coin, value_in_cents(coin));
+    }
 
-    let person = Person::new("Luffy", 19);
-    println!("{}", person.greet());
+    // Exercise 2
+    let messages = vec![
+        Message::Quit,
+        Message::Move { x: 10, y: 20 },
+        Message::Write(String::from("hello")),
+        Message::ChangeColor(255, 0, 128),
+    ];
+    for msg in &messages {
+        process(msg);
+    }
 
-    let mut stack = Stack::new();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    stack.pop();
-    println!("{}", stack.is_empty());
+    if let Some(i) = divide(10.0, 2.0) {
+        println!("{}", i)
+    } else {
+        println!("None")
+    }
+    if let Some(i) = divide(10.0, 0.0) {
+        println!("{}", i)
+    } else {
+        println!("None")
+    }
 }
